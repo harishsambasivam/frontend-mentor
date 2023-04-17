7f9b7af2368bef525c822e0445c76f6755b7ee2c
harishsambasivam <imharishsambasivam@gmail.com>
2023-04-17 19:41:10 +0530

    feat: colors and dynamic content

--- social-media-dashboard-with-theme-switcher-master/code/src/App.tsx
+++ social-media-dashboard-with-theme-switcher-master/code/src/App.tsx
@@ -1,19 +1,44 @@
 import { ReactNode } from "react";
 import "./App.css";
 import facebookIcon from "./assets/images/icon-facebook.svg";
+import twitterIcon from "./assets/images/icon-twitter.svg";
+import instagramIcon from "./assets/images/icon-instagram.svg";
+import youtubeIcon from "./assets/images/icon-youtube.svg";
+import downIcon from "./assets/images/icon-down.svg";
+import upIcon from "./assets/images/icon-up.svg";
 
-const FollowersCard: any = ({ handle, followers, difference }: any) => {
+const FollowersCard: any = ({
+  handle,
+  followers,
+  difference,
+  borderStyles = "border-cyan-700",
+  icon,
+  followersText = "followers",
+  countIncreased = true,
+}: any) => {
   return (
-    <div className="flex flex-col items-center justify-around m-4 border-t-4 rounded-lg border-cyan-700 bg-slate-100 h-52 md:flex-grow">
-      <div className="handle">
-        <p className="font-semibold text-slate-600"> {handle}</p>
-      </div>
-      <div className="flex flex-col items-center justify-center counter ">
-        <span className="text-4xl font-bold ">{followers}</span>
-        <p className="text-xs font-light uppercase tracking-w">followers</p>
-      </div>
-      <div className="font-semibold difference text-primary-limeGreen">
-        {difference} Today
+    <div className={`${borderStyles} w-full h-52 m-4 md:flex-grow rounded-md`}>
+      <div
+        className={` flex flex-col items-center justify-around mt-1 rounded-md  bg-slate-100 h-56 md:flex-grow `}
+      >
+        <div className="flex items-center mt-2">
+          <img src={icon} className="w-5 h-5" />
+          <p className="ml-2 font-semibold text-slate-600"> {handle}</p>
+        </div>
+        <div className="flex flex-col items-center justify-center counter ">
+          <span className="text-5xl font-bold ">{followers}</span>
+          <p className="mt-1 text-xs font-light uppercase tracking-w">
+            {followersText}
+          </p>
+        </div>
+        <div
+          className={`font-bold difference ${
+            countIncreased ? "text-primary-limeGreen" : "text-primary-brightRed"
+          } flex items-center gap-2`}
+        >
+          <img src={countIncreased ? upIcon : downIcon} className="w-2 h-1" />
+          {difference} Today
+        </div>
       </div>
     </div>
   );
@@ -21,12 +46,17 @@ const FollowersCard: any = ({ handle, followers, difference }: any) => {
 
 type ReactionCardProps = {
   title: string;
-  count: number;
+  count: string;
   icon: string;
-  stat: number;
+  deviationInPercent: number;
 };
 
-const ReactionCard = ({ title, count, icon, stat }: ReactionCardProps) => {
+const ReactionCard = ({
+  title,
+  count,
+  icon,
+  deviationInPercent,
+}: ReactionCardProps) => {
   return (
     <div className="flex items-center h-32 p-5 bg-slate-50">
       <div className="flex flex-col justify-between h-full">
@@ -37,8 +67,18 @@ const ReactionCard = ({ title, count, icon, stat }: ReactionCardProps) => {
         <div className="logo">
           <img src={icon} className="w-5 h-5" />
         </div>
-        <div className="text-xs font-semibold stats text-primary-limeGreen">
-          {stat}%
+        <div
+          className={`text-xs font-semibold stats ${
+            deviationInPercent < 0
+              ? "text-primary-brightRed"
+              : "text-primary-limeGreen"
+          } flex items-center gap-2`}
+        >
+          <img
+            src={deviationInPercent < 0 ? downIcon : upIcon}
+            className="w-2 h-1"
+          />
+          {deviationInPercent}%
         </div>
       </div>
     </div>
@@ -51,10 +91,10 @@ function App() {
       {/* top card */}
       <div className="flex flex-col h-64 p-4 top-card bg-slate-50 rounded-b-2xl md:rounded-none md:flex-row md:justify-center">
         <div className="p-4 border-b-2 md:border-none">
-          <div className="text-2xl font-bold heading text-slate-800">
+          <div className="text-3xl font-bold heading text-slate-800">
             Social Media Dashboard
           </div>
-          <div className="font-semibold followers-count text-slate-500">
+          <div className="mt-1 text-sm font-semibold followers-count text-slate-500">
             Total Followers: 23,004
           </div>
         </div>
@@ -62,10 +102,37 @@ function App() {
       </div>
       {/* followers cards */}
       <div className="flex flex-col m-4 -mt-20 md:flex-row md:-mt-40">
-        <FollowersCard handle="@nathanf" followers={1987} difference={12} />
-        <FollowersCard handle="@nathanf" followers={1987} difference={12} />
-        <FollowersCard handle="@nathanf" followers={1987} difference={12} />
-        <FollowersCard handle="@nathanf" followers={1987} difference={12} />
+        <FollowersCard
+          handle="@nathanf"
+          followers={1987}
+          difference={12}
+          borderStyles="bg-primary-facebook"
+          icon={facebookIcon}
+        />
+        <FollowersCard
+          handle="@nathanf"
+          followers={1044}
+          difference={99}
+          borderStyles="bg-primary-twitter"
+          icon={twitterIcon}
+        />
+
+        <FollowersCard
+          handle="@realnathanf"
+          followers={"11k"}
+          difference={1099}
+          borderStyles="bg-gradient-to-r from-primary-instagram-gradient-start to-primary-instagram-gradient-end"
+          icon={instagramIcon}
+        />
+        <FollowersCard
+          handle="@nathanf"
+          followers={1987}
+          difference={12}
+          borderStyles="bg-primary-youtube"
+          icon={youtubeIcon}
+          followersText="subscribers"
+          countIncreased={false}
+        />
       </div>
 
       {/* reactions */}
@@ -76,46 +143,56 @@ function App() {
           </p>
           <div className="grid gap-4 mt-4 cards md:grid-cols-4">
             <ReactionCard
-              count={100}
+              count={"100"}
               icon={facebookIcon}
               title="Page Views"
-              stat={87}
+              deviationInPercent={3}
             />
             <ReactionCard
-              count={100}
+              count={"52"}
               icon={facebookIcon}
-              title="Page Views"
-              stat={87}
+              title="Likes"
+              deviationInPercent={2}
             />
             <ReactionCard
-              count={100}
-              icon={facebookIcon}
-              title="Page Views"
-              stat={87}
+              count={"5462"}
+              icon={instagramIcon}
+              title="Likes"
+              deviationInPercent={2257}
+            />
+
+            <ReactionCard
+              count={"52k"}
+              icon={instagramIcon}
+              title="Profile Views"
+              deviationInPercent={1375}
+            />
+
+            <ReactionCard
+              count={"117"}
+              icon={twitterIcon}
+              title="Retweets"
+              deviationInPercent={303}
+            />
+
+            <ReactionCard
+              count={"507"}
+              icon={twitterIcon}
+              title="Likes"
+              deviationInPercent={553}
             />
             <ReactionCard
-              count={100}
-              icon={facebookIcon}
-              title="Page Views"
-              stat={87}
+              count={"107"}
+              icon={youtubeIcon}
+              title="Likes"
+              deviationInPercent={-19}
             />
+
             <ReactionCard
-              count={100}
-              icon={facebookIcon}
-              title="Page Views"
-              stat={87}
-            />
-            <ReactionCard
-              count={100}
-              icon={facebookIcon}
-              title="Page Views"
-              stat={87}
-            />
-            <ReactionCard
-              count={100}
-              icon={facebookIcon}
-              title="Page Views"
-              stat={87}
+              count={"1407"}
+              icon={youtubeIcon}
+              title="Total Views"
+              deviationInPercent={-12}
             />
           </div>
         </div>
--- social-media-dashboard-with-theme-switcher-master/code/tailwind.config.js
+++ social-media-dashboard-with-theme-switcher-master/code/tailwind.config.js
@@ -20,8 +20,8 @@ export default {
           facebook: "hsl(208, 92%, 53%)",
           twitter: "hsl(203, 89%, 53%)",
           youtube: "hsl(348, 97%, 39%)",
-          instagram:
-            "linear-gradient(to right, hsl(37, 97%, 70%), hsl(329, 70%, 58%))",
+          "instagram-gradient-start": "hsl(37, 97%, 70%)",
+          "instagram-gradient-end": "hsl(329, 70%, 58%)",
         },
         neutral: {
           veryDarkBlue: {
